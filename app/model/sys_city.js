e9d440c20851c6637872c4bc2f7792cc093d11b1
893281878@qq.com <893281878@qq.com>
2025-08-21 20:06:03 +0800

    reset mysql

--- app/model/sys_city.js
+++ app/model/sys_city.js
@@ -4,23 +4,23 @@ module.exports = app => {
   const DataTypes = app.Sequelize;
   const SysCity = app.model.define('sys_city', {
     createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW, comment: '创建时间' },
-    id: { type: DataTypes.INTEGER, autoIncrement:true, primaryKey: true, comment: '主键' },
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, comment: '主键' },
     updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW, comment: '更新时间' },
     name: { type: DataTypes.STRING, comment: '城市名' },
     city_id: { type: DataTypes.STRING, comment: '城市id' },
-    province_id: { type: DataTypes.STRING, comment: '关联省份id' },
-  },{
-  
-  paranoid: false,
-});
+    province_id: { type: DataTypes.STRING(12), comment: '关联省份id' },
+  }, {
+
+    paranoid: false,
+  });
   SysCity.associate = function() {
-       app.model.SysProvince.hasMany(app.model.SysCity, {
-            foreignKey: 'province_id',
-            sourceKey: 'province_id',
-            constraints: true,
-          });
-          
-      };
-  //SysCity.sync({ alter: true });
+    app.model.SysProvince.hasMany(app.model.SysCity, {
+      foreignKey: 'province_id',
+      sourceKey: 'province_id',
+      constraints: true,
+    });
+
+  };
+  // SysCity.sync({ alter: true });
   return SysCity;
 };
