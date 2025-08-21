d54109133d4e64e3225df8521dab5349073f7663
893281878@qq.com <893281878@qq.com>
2025-08-21 20:40:18 +0800

    adj pm2 logs

--- pm2.config.js
+++ pm2.config.js
@@ -18,8 +18,8 @@ module.exports = {
       interpreter: 'node',
       watch: true,
       max_memory_restart: '500M',
-      error_file: './logs/pm2-error.log',
-      out_file: './logs/pm2-out.log',
+      error_file: '../logs/pm2-error.log',
+      out_file: '../logs/pm2-out.log',
       merge_logs: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       // 新增如下行
