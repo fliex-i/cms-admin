db3b2feb45e75cd86813879965a189ece439c240
893281878@qq.com <893281878@qq.com>
2025-09-29 15:22:16 +0800

    add service sql

--- mysql/mc_service.sql
+++ mysql/mc_service.sql
@@ -30,7 +30,7 @@ CREATE TABLE `mc_service` (
   `message` text COMMENT '咨询消息',
   `response` text COMMENT '管理员恢复消息',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
